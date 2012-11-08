7159f6b0218e55e3d4f9f7d5100e51825045a1eb
june.chan <june.chan@embracesource.com>
2012-11-08 18:40:01 +0800

    Signed-off-by: june.chan <june.chan@embracesource.com>

--- src/org/test/hotrod/HotRod.java
+++ src/org/test/hotrod/HotRod.java
@@ -1,17 +1,20 @@
 package org.test.hotrod;
 
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
-import org.infinispan.api.BasicCache;
-import org.infinispan.api.BasicCacheContainer;
+import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.VersionedValue;
 
 public class HotRod extends AbstractJavaSamplerClient {
-	private static BasicCacheContainer container = null;
+	private static RemoteCacheManager container = null;
+	private static Lock lock = new ReentrantLock();
 	private String cacheName = null;
 	private String putOrGet;
 	private String value;
@@ -21,59 +24,74 @@ public class HotRod extends AbstractJavaSamplerClient {
 	@Override
 	public void setupTest(JavaSamplerContext context) {
 		super.setupTest(context);
-		if(container==null){
-			container = new RemoteCacheManager();
+
+		if (container == null) {
+			try {
+				lock.lock();
+				if (container == null) {
+					container = new RemoteCacheManager();
+					System.out.println("init container");
+				}
+			} finally {
+				lock.unlock();
+			}
 		}
-		
+
 		cacheName = context.getParameter("cacheName", "");
 		putOrGet = context.getParameter("putOrGet", "put");
 		keyLength = context.getIntParameter("keyLength", 150);
 		keyRang = context.getIntParameter("keyRang", 100000);
 		int size = context.getIntParameter("size", 1024);
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < size; i++) {
-			sb.append(i % 100);
+
+		if (value == null) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < size; i++) {
+				sb.append(i % 100);
+			}
+			value = sb.toString();
 		}
-		value = sb.toString();
-		System.out.println("cacheName : " + cacheName);
-		System.out.println("size : " + size);
-		System.out.println("keyLength : " + keyLength);
-		System.out.println("keyRang : " + keyRang);
-		System.out.println("putOrGet : " + putOrGet);
 	}
 
 	@Override
 	public void teardownTest(JavaSamplerContext context) {
 		super.teardownTest(context);
+		System.out.println("stop container");
 		container.stop();
-		container = null;
+		container=null;
 	}
 
-	public  String repeat(char ch, int repeat) {
+	public String repeat(char ch, int repeat) {
 		char[] buf = new char[repeat];
 		for (int i = repeat - 1; i >= 0; i--) {
 			buf[i] = ch;
 		}
 		return new String(buf);
 	}
-	
+
 	@Override
 	public SampleResult runTest(JavaSamplerContext arg0) {
 		SampleResult sr = new SampleResult();
 		sr.setSampleLabel("hotrod-" + putOrGet);
-		BasicCache<String, Object> cache = null;
+		RemoteCache<String, Object> cache = null;
 		if (!cacheName.equals("")) {
 			cache = container.getCache(cacheName);
 		} else {
 			cache = container.getCache();
 		}
-		int in=new Random().nextInt(keyRang);
-		String key = repeat('a', keyLength-Integer.toString(in).length()) + in;
+		int in = new Random().nextInt(keyRang);
+		String key = repeat('a', keyLength - Integer.toString(in).length())
+				+ in;
 		try { // 这里调用我们要测试的java类，这里我调用的是一个Test类
 			sr.sampleStart(); // 记录程序执行时间，以及执行结果
 			Object val = null;
 			if (putOrGet.equals("put")) {
-				cache.put(key, value);
+				VersionedValue<String> versionedValue=cache.getVersioned(key);
+				if(versionedValue!=null){
+					cache.replaceWithVersion(key, value, versionedValue.getVersion());
+				}else{
+					cache.put(key, value);
+				}
+				
 				sr.setSuccessful(true);
 			} else {
 				val = cache.get(key);
@@ -82,6 +100,7 @@ public class HotRod extends AbstractJavaSamplerClient {
 		} catch (Throwable e) {
 			System.out.println("Exception is " + e.getMessage());
 			sr.setSuccessful(false);
+			sr.setErrorCount(1);
 		} finally {
 			sr.sampleEnd();
 		}
